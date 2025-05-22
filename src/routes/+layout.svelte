fb7bcae21e729cc03821800e202bced3f27c51e9
Travis Umbel <Umbelth94@gmail.com>
2025-05-21 19:08:22 -0500

    added interface models

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 
+	
+
 	let { children } = $props();
 </script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
 
 {@render children()}
